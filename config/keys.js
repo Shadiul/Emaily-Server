c6c25e9a20b4db486bab9878424e82f374145905
Shadiul Huda <shadiul.71@gmail.com>
2020-11-05 05:13:59 +0600

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '370849374682-42uitcrcmpmbf049a08qb3imcpjjm6qs.apps.googleusercontent.com',
-  googleClientSecret: 'UlIccyJYrGuvoGJBfVxFqXTI',
-  mongoURI:
-    'mongodb+srv://shadiul:KtkXiV6cXHB68y6B@emaily.2pt5c.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: process.env.COOKIE_KEY,
-};
